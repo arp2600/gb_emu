57b637a8cd697e0d71980be9505c87e523837b57
George Roger <georger@xmos.com>
2018-05-30 20:53:40 +0100

    opcode_examiner now print more information

--- scripts/opcode_examiner.py
+++ scripts/opcode_examiner.py
@@ -16,17 +16,34 @@ def main():
         for i in range(0, len(master_code)):
             if master_code[i] != bc[i]:
                 master_code[i] = 'x'
-        print("0x{:02x} 0b{:08b}".format(oc, oc))
+        # print("0x{:02x} 0b{:08b}".format(oc, oc))
 
     out = ''
     was_x = master_code[0] == 'x'
+    master_opcode = 0
     for i in range(0, len(master_code)):
         is_x = master_code[i] == 'x'
+
+        master_opcode = master_opcode << 1
+        master_opcode += is_x
+
         if is_x != was_x:
             out += '_'
             was_x = is_x
         out += master_code[i]
+    # print('opcode format = {}'.format(out))
+
+    for oc in opcodes:
+        masked = oc & master_opcode
+        masked = masked >> trailing_zeros(master_opcode)
+        print("0x{:02x} 0b{:08b} {}".format(oc, oc, masked))
     print('opcode format = {}'.format(out))
+
+
+def trailing_zeros(x):
+    s = bin(x)
+    return len(s) - len(s.rstrip('0'))
+
 
 
 if __name__ == '__main__':
