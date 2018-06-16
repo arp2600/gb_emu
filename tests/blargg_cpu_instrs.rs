02cf9dd23d7c10ac761b7ff6f66fe7953c043277
George Roger <georger@xmos.com>
2018-06-17 00:09:50 +0100

    Changed timeout cycles on some tests

--- tests/blargg_cpu_instrs.rs
+++ tests/blargg_cpu_instrs.rs
@@ -1,70 +1,73 @@
 extern crate gb_emu;
 use gb_emu::Emulator;
+use std::path::Path;
 use std::str;
 
 #[test]
 fn special() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/01-special.gb");
+    run_test_rom("individual/01-special.gb", 3_000_000);
 }
 
 #[test]
 fn interrupts() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/02-interrupts.gb");
+    run_test_rom("individual/02-interrupts.gb", 3_000_000);
 }
 
 #[test]
 fn op_sp_hl() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/03-op_sp_hl.gb");
+    run_test_rom("individual/03-op_sp_hl.gb", 3_000_000);
 }
 
 #[test]
 fn op_r_imm() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/04-op_r_imm.gb");
+    run_test_rom("individual/04-op_r_imm.gb", 3_000_000);
 }
 
 #[test]
 fn op_rp() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/05-op_rp.gb");
+    run_test_rom("individual/05-op_rp.gb", 3_000_000);
 }
 
 #[test]
 fn ld_r_r() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/06-ld_r_r.gb");
+    run_test_rom("individual/06-ld_r_r.gb", 3_000_000);
 }
 
 #[test]
 fn jr_jp_call_ret_rst() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/07-jr_jp_call_ret_rst.gb");
+    run_test_rom("individual/07-jr_jp_call_ret_rst.gb", 3_000_000);
 }
 
 #[test]
 fn misc_instrs() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/08-misc_instrs.gb");
+    run_test_rom("individual/08-misc_instrs.gb", 3_000_000);
 }
 
 #[test]
 fn op_r_r() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/09-op_r_r.gb");
+    run_test_rom("individual/09-op_r_r.gb", 3_000_000);
 }
 
 #[test]
 fn bit_ops() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/10-bit_ops.gb");
+    run_test_rom("individual/10-bit_ops.gb", 7_000_000);
 }
 
 #[test]
 fn op_a_hl() {
-    run_test_rom("blargg_test_roms/cpu_instrs/individual/11-op_a_hl.gb");
+    run_test_rom("individual/11-op_a_hl.gb", 3_000_000);
 }
 
 #[test]
 fn cpu_instrs() {
-    run_test_rom("blargg_test_roms/cpu_instrs/cpu_instrs.gb");
+    run_test_rom("cpu_instrs.gb", 3_000_000);
 }
 
-fn run_test_rom(test_rom_path: &str) {
-    let mut emulator = Emulator::new(None, test_rom_path);
-    for _ in 0..3000_000 {
+fn run_test_rom(test_rom: &str, max_cycles: u64) {
+    let test_rom_path = Path::new("blargg_test_roms/cpu_instrs").join(test_rom);
+
+    let mut emulator = Emulator::new(None, test_rom_path.to_str().unwrap());
+    for _ in 0..max_cycles {
         emulator.tick()
     }
 
