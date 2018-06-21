8d85e6d49b9f2657066bf7d5ddea37c2c07bf998
George Roger <georger@xmos.com>
2018-06-21 21:11:04 +0100

    Fixing broken tests

--- src/lcd.rs
+++ src/lcd.rs
@@ -10,6 +10,7 @@ pub struct LCD {
     update_time: u64,
     enabled: bool,
     frame: u64,
+    next_ly: u8,
 }
 
 impl LCD {
@@ -18,6 +19,7 @@ impl LCD {
             update_time: 0,
             enabled: false,
             frame: 0,
+            next_ly: 0,
         }
     }
 
@@ -25,13 +27,12 @@ impl LCD {
         let enabled = memory.get_io(IoRegs::LCDC) & 0b1000_0000 != 0;
         if enabled && !self.enabled {
             self.enabled = true;
-            // 455 means this is the first cycle
-            // this is to pass testing against old lcd implementation
-            self.update_time = cycles + 455;
+            self.update_time = cycles;
+            self.next_ly = 0;
             memory.set_io(IoRegs::LY, 0);
         }
 
-        if self.enabled && cycles > self.update_time {
+        if self.enabled && cycles >= self.update_time {
             self.update_time += 456;
 
             let ly = memory.get_io(IoRegs::LY);
@@ -56,7 +57,8 @@ impl LCD {
                 memory.set_io(IoRegs::STAT, 0b10);
             }
 
-            memory.set_io(IoRegs::LY, ly.wrapping_add(1));
+            memory.set_io(IoRegs::LY, self.next_ly);
+            self.next_ly = self.next_ly.wrapping_add(1) % 154;
         }
     }
 }
@@ -85,10 +87,14 @@ mod tests {
             let frame_time = cycles % (456 * 154);
             let test_ly = (frame_time / 456) as u8;
 
-            lcd.tick(&mut memory, cycles);
+            lcd.tick(&mut memory, cycles, None);
             let lcd_ly = memory.get_io(IoRegs::LY);
 
-            assert_eq!(lcd_ly, test_ly);
+            assert_eq!(
+                lcd_ly, test_ly,
+                "lcd {} != test {} at cycles {}",
+                lcd_ly, test_ly, cycles
+            );
         }
     }
 }
