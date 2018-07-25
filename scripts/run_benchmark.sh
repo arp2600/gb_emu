8097e36c90eb442e929177b826067e16d102d140
George Roger <georger@xmos.com>
2018-07-25 22:53:00 +0100

    Updated benchmark script to use --release flag.

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-bench=$(cargo run --example benchmark 2> /dev/null | sed -n -e 's/^_BENCH_ //p')
+bench=$(cargo run --example benchmark --release 2> /dev/null | sed -n -e 's/^_BENCH_ //p')
 
 date >> benchmark_results
 echo "$bench" >> benchmark_results
